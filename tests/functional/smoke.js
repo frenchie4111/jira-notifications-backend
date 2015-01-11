82444d090cbaa4d8abd7254fd430748c62e78c11
Mike Lyons <mlyons@mkone.co>
2015-01-11 12:34:59 -0800

    Added stub files and directories

---
+++ tests/functional/smoke.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    var chai = require( 'chai' ),
+        assert = chai.assert;
+
+    describe( 'Smoke test', function() {
+        it( 'Should pass', function() {
+            assert.isTrue( true, 'Smoke test!' );
+        } );
+    } )
+
+})();
