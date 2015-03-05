f21a22ba8860b7778cfb5632210655ca575ce01d
Michael Lyons <mdl0394@gmail.com>
2015-03-05 01:07:45 -0800

    Added model edit

--- app/models/index.js
+++ app/models/index.js
@@ -9,6 +9,31 @@
 
     var previously_initialized = false;
 
+    var _edit = function( values, opts ) {
+        var _this = this;
+
+        opts = opts || {};
+
+        // Since the replaceNestedFields class method bound to the wrong place, we have to bind it to this model so it can see nestedFieldsInformation
+        // TODO: Find a better way to bind parent static methods to child model instance
+        var model_name = _this.__options.name;
+        if( _.isObject( model_name ) ) model_name = model_name.singular;
+
+        var model = _this.__options.sequelize.models[ model_name ];
+
+        var default_opts = {
+            fields: model.editableFields
+        };
+        _.defaults( opts, default_opts );
+
+        return _this
+            .updateAttributes( values, opts );
+    };
+
+    var defaultInstanceMethods = {
+        edit: _edit
+    };
+
     module.exports.init = function init( opts ) {
         if( previously_initialized ) return;
         previously_initialized = true;
@@ -23,7 +48,10 @@
                     port: opts.database_port,
                     host: opts.database_url,
                     logging: opts.sequelize_logging,
-                    native: opts.native
+                    native: opts.native,
+                    define: {
+                        instanceMethods: defaultInstanceMethods
+                    }
                 } )
             ;
 
